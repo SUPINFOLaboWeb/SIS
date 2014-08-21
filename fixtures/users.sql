bcdbe20b05a5a0600670df7fe25e3982992a2c97
jeromepin <jeromepin38@gmail.com>
2014-08-21 17:12:36 +0200

    add fixtures files and folders

---
+++ fixtures/users.sql
@@ -0,0 +1,5 @@
+INSERT INTO `users` (`id_booster`, `campus_id`, `privilege_id`, `lastname`, `firstname`, `password`) VALUES
+(1, 1, 1, 'SUPERADMIN', 'Superadmin', '$2a$08$sKFrhqYya89ma89yg5Ip7uTO4R0AoHMpQLUZdWNmb522q2H9JCe7W'),
+(2, 2, 2, 'ADMIN', 'Admin', '$2a$08$a6Sd.4kXXhd6zvfzcSnfsudHVblbKFT/dQ1nVhf45vnbNu1twHMVm'),
+(3, 3, 3, 'NORMAL', 'Normal', '$2a$08$aZQe/5J65j/0NrURoSxJYeIhoLFvj8DplI2vqptzoTUicaHsbIRMi'),
+(4, 4, 4, 'INACTIF', 'Inactif', '$2a$08$8tKMQ3UOYSgcTIynPT47de9RosO2JGV2wSQXmp8PA3zm4X.aCXykq');
