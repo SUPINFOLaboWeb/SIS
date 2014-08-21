374eca7a56781f8ce238178c7276cd2b50caaeab
jeromepin <jeromepin38@gmail.com>
2014-08-21 17:11:53 +0200

    add login feature

--- pages/authentication.php
+++ pages/authentication.php
@@ -2,6 +2,7 @@
 	
 	require_once '../config.php';
 	require_once '../includes/managers.php';
+	require_once '../classes/PasswordHash.php';
 
 	if (isset($_POST['idBooster']) && isset($_POST['password'])) {
 		
@@ -11,9 +12,25 @@
 		$pdoUserManager = new PdoUserManager;
 		$users = $pdoUserManager->getUsers();
 
+		$hasher = new PasswordHash(8, FALSE);
+
+		// A revoir : boucle for(each) non optimale pour une recherche
+
+		foreach ($users as $user) {
+			if (($idBooster == $user->getIdBooster()) && ($hasher->CheckPassword($password, $user->getPassword()))) {
+				
+				if ($user->getPrivilegeId() != 4) {
+					header('Location: home.php') or die();
+				} else {
+					header('Location: login.php?erno=3') or die();
+				}
+			}
+		}
+
+		header('Location: login.php?erno=2') or die();
 
 	} else {
-
+		header('Location: login.php?erno=1') or die();
 	}
 
 ?>
