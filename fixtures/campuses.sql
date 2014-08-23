707788c6d46405d6e27cf1c0924c2efc7da79030
jeromepin <jeromepin38@gmail.com>
2014-08-23 19:11:22 +0200

    add fixtures for campus, news & privileges

---
+++ fixtures/campuses.sql
@@ -0,0 +1,6 @@
+INSERT INTO `campuses` (`id`, `campus_name`) VALUES
+(1, 'CLERMONT-FERRAND'),
+(2, 'GRENOBLE'),
+(3, 'MARSEILLE'),
+(4, 'MONTPELLIER'),
+(5, 'NICE');
---
+++ fixtures/privileges.sql
@@ -0,0 +1,5 @@
+INSERT INTO `privileges` (`id`, `privilege_name`) VALUES
+(1, 'SUPERADMIN'),
+(2, 'ADMIN'),
+(3, 'NORMAL'),
+(4, 'INACTIF');
