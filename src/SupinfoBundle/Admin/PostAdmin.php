08c88a27c82beef4c850b6ab8a1645983084594d
Fabien Papet <fabien.papet@gmail.com>
2015-07-15 23:25:38 +0200

    Admin news

---
+++ src/SupinfoBundle/Admin/PostAdmin.php
@@ -0,0 +1,43 @@
+<?php
+namespace SupinfoBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class PostAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('title')
+            ->add('content')
+            ->add('createdBy')
+            ->add('createdAt')
+            ->add('modifiedBy')
+            ->add('campus')
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('title')
+            ->add('createdBy')
+            ->add('createdAt')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id')
+            ->add('title')
+            ->add('createdBy')
+        ;
+    }
+}
---
+++ src/SupinfoBundle/Entity/Post.php
@@ -0,0 +1,235 @@
+<?php
+
+namespace SupinfoBundle\Entity;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+use UserBundle\Entity\UserAccount;
+
+/**
+ * Post
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="SupinfoBundle\Entity\PostRepository")
+ */
+class Post
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+    /**
+     * @var DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime",nullable=true)
+     */
+    private $createdAt;
+
+    /**
+     * @var DateTime
+     *
+     * @ORM\Column(name="modifiedAt", type="datetime" , nullable=true)
+     */
+    private $modifiedAt;
+
+    /**
+     * @var UserAccount
+     * @ORM\ManyToOne(targetEntity="UserBundle\Entity\UserAccount")
+     * @ORM\JoinColumn()
+     */
+    private $createdBy;
+
+    /**
+     * @var UserAccount
+     * @ORM\ManyToOne(targetEntity="UserBundle\Entity\UserAccount")
+     * @ORM\JoinColumn(nullable=true)
+     */
+    private $modifiedBy;
+
+    /**
+     * @var UserAccount
+     * @ORM\ManyToOne(targetEntity="SupinfoBundle\Entity\Campus")
+     * @ORM\JoinColumn(nullable=true)
+     */
+    private $campus;
+
+
+    public function __construct(){
+        $this->createdAt = new DateTime();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Post
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return Post
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param DateTime $createdAt
+     * @return Post
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set modifiedAt
+     *
+     * @param DateTime $modifiedAt
+     * @return Post
+     */
+    public function setModifiedAt($modifiedAt)
+    {
+        $this->modifiedAt = $modifiedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get modifiedAt
+     *
+     * @return DateTime
+     */
+    public function getModifiedAt()
+    {
+        return $this->modifiedAt;
+    }
+
+    /**
+     * @return UserAccount
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * @param UserAccount $createdBy
+     * @return $this
+     */
+    public function setCreatedBy(UserAccount $createdBy)
+    {
+        $this->createdBy = $createdBy;
+        return $this;
+    }
+
+    /**
+     * @return UserAccount
+     */
+    public function getModifiedBy()
+    {
+        return $this->modifiedBy;
+    }
+
+    /**
+     * @param UserAccount $modifiedBy
+     * @return $this
+     */
+    public function setModifiedBy(UserAccount $modifiedBy)
+    {
+        $this->modifiedBy = $modifiedBy;
+        return $this;
+    }
+
+    /**
+     * @return UserAccount
+     */
+    public function getCampus()
+    {
+        return $this->campus;
+    }
+
+    /**
+     * @param Campus $campus
+     * @return $this
+     */
+    public function setCampus(Campus $campus)
+    {
+        $this->campus = $campus;
+        return $this;
+    }
+}
